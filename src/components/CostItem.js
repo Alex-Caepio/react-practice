b82d91288b631bca940c819221005df1f0b918df
Aleksei Brutov <mjbcaepio@gmail.com>
2024-04-19 15:22:45 +0300

    an initial commit of a project

--- src/components/CostItem.js
+++ src/components/CostItem.js
@@ -1,16 +1,13 @@
 import "./CostItem.css";
+import CostDate from "./CostDate";
 
 function CostItem(props) {
-  const costDate = new Date(2021, 2, 12);
-  const costDescription = "Холодильник";
-  const costAmount = 999.99;
-
   return (
     <div className="cost-item">
-      <div>{costDate.toISOString}</div>
+      <CostDate date={props.date} />
       <div className="cost-item__description">
-        <h2>{costDescription}</h2>
-        <div className="cost-item__price">${costAmount}</div>
+        <h2>{props.description}</h2>
+        <div className="cost-item__price">${props.amount}</div>
       </div>
     </div>
   );
