fc85257b8000c6fbe3d153d84c401e535f0dffb5
aleksei <mjbcaepio@gmail.com>
2024-04-21 23:28:51 +0300

    Сделал основной функционал задачи

--- src/App.js
+++ src/App.js
@@ -1,43 +1,7 @@
-import CostItem from "./components/CostItem";
+import Costs from "./components/Costs";
 
 function App() {
-  const costs = [
-    {
-      date: new Date(2021, 2, 12),
-      description: "Холодильник",
-      amount: 999.99,
-    },
-    {
-      date: new Date(2021, 11, 25),
-      description: "MacBook",
-      amount: 1254.72,
-    },
-    {
-      date: new Date(2021, 4, 1),
-      description: "Джинсы",
-      amount: 49.99,
-    },
-  ];
-
-  return (
-    <div>
-      <CostItem
-        date={costs[0].date}
-        description={costs[0].description}
-        amount={costs[0].amount}
-      ></CostItem>
-      <CostItem
-        date={costs[1].date}
-        description={costs[1].description}
-        amount={costs[1].amount}
-      ></CostItem>
-      <CostItem
-        date={costs[2].date}
-        description={costs[2].description}
-        amount={costs[2].amount}
-      ></CostItem>
-    </div>
-  );
+  return <Costs />;
 }
 
 export default App;
